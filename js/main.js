13082cd5348d9da1e2891f9bae4037f7e1e53f2f
90dandan <1brokendrumhead@gmail.com>
2020-02-07 14:00:28 -0600

    everything but the win

--- js/main.js
+++ js/main.js
@@ -7,8 +7,9 @@
 // Variables might include (board/turn/winner)
 //let gameMarker = "X";
 //let board = document.querySelectorAll('.board');
-let board, turn = 1;
-//let player = ("1", "-1");
+let board, turn = 1; 
+winner = false;
+
 /*------Cached Element References------*/
 
 // You might choose to put your game status here
@@ -19,41 +20,71 @@ let board, turn = 1;
 // This is where you should put the event listener
 // for a mouse-click
 document.getElementById('squares').addEventListener('click', onClick);
-
+//document.getElementById("rst").addEventListener('click', init);
 /*------Functions------*/
 
 init();
 
 function init() {
-    board = ['sq','sq','sq','sq','sq','sq','sq','sq','sq']
+    board = [null,null,null,null,null,null,null,null,null];
     turn = 1;
+    document.getElementById("message").innerHTML = "X's Turn!";
+    winner=false;
+}
+
+function onClick(event) {
+    if (winner===false) {
+    let squareIdx = parseInt((event.target.id).replace("sq",""));
+    console.log(board);
+    if (board[squareIdx] !== null) return;
+    else if (turn===1) { 
+        board[squareIdx]=turn;
+        document.getElementById(event.target.id).innerHTML = "X"; 
+        document.getElementById("message").innerHTML = "O's Turn!"; 
+        togglePlayer(); 
+    } else if(turn===-1){ 
+        board[squareIdx]=turn;
+        document.getElementById(event.target.id).innerHTML = "O";
+        document.getElementById("message").innerHTML = "X's Turn!";
+        togglePlayer();
+    }
+    }
+}
+
+function togglePlayer() {
+    if (winner===false){
+    isWinner();
+    turn *= -1;
+    console.log('turn');
+    }
 }
 
 function isWinner() {
-    if (board[0]+board[1]+board[2] === 3 || board[0]+board[4]+board[8] === 3 || board[0]+board[3]+board[6] === 3 || board[2]+board[4]+board[6] === 3 || board[2]+board[5]+board[8] === 3 || board[6]+board[7]+board[8] === 3 || board[3]+board[4]+board[5] === 3)
+    if (board[0]+board[1]+board[2] === 3 || 
+        board[0]+board[4]+board[8] === 3 || 
+        board[0]+board[3]+board[6] === 3 || 
+        board[2]+board[4]+board[6] === 3 || 
+        board[2]+board[5]+board[8] === 3 || 
+        board[6]+board[7]+board[8] === 3 || 
+        board[3]+board[4]+board[5] === 3)
     {
-        document.getElementById("message").innerHtml = ("Player One Wins!!")
-    } else if (board[0]+board[1]+board[2] === 3 || board[0]+board[4]+board[8] === 3 || board[0]+board[3]+board[6] === 3 || board[2]+board[4]+board[6] === 3 || board[2]+board[5]+board[8] === 3 || board[6]+board[7]+board[8] === 3 || board[3]+board[4]+board[5] === 3)
+        winner = true; 
+        document.getElementById("message").innerHtml = "Player One Wins!!"; 
+        console.log("win")
+    } else if(board[0]+board[1]+board[2] === -3 || 
+            board[0]+board[4]+board[8] === -3 || 
+            board[0]+board[3]+board[6] === -3 || 
+            board[2]+board[4]+board[6] === -3 || 
+            board[2]+board[5]+board[8] === -3 || 
+            board[6]+board[7]+board[8] === -3 || 
+            board[3]+board[4]+board[5] === -3)
     {
-        document.getElementById("message").innerHtml = ("Player Two Wins!!")
-    } else togglePlayer();
+        winner = true;
+        document.getElementById("message").innerHtml = "Player Two Wins!!";
+        console.log('win')
+    }  //console.log('isWinner')
 } 
 
-
-function onClick(event) {
-    let squareIdx = event.target.id.replace("sq","");
-    if (board[squareIdx]===1) {
-        document.getElementById(squareIdx).innerHTML = "X";
-    } else if (board[squareIdx]===-1) {
-        document.getElementById(squareIdx).innerHTML = "O";
-    } else  isWinner();
-}
-
-
-
-function togglePlayer() {
-    turn *= -1
-}
 
 
 
